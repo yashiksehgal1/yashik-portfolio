1339c0d8383883208585ddd15319e7d941e9ffba
Yashik <yashik604@gmail.com>
2026-02-13 19:02:27 +0530

    Updated Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,109 +13,79 @@ const Projects = () => {
           {/* CARD 1 */}
           <div className="relative group md:row-span-3 overflow-hidden rounded-3xl">
 
-            <div className="absolute inset-0 border-t-[25px] border-l-[25px] 
-                            border-white/20 rounded-3xl z-10 pointer-events-none" />
+            <div className="absolute inset-0 border-t-[25px] border-l-[25px] border-white/20 rounded-3xl z-10 pointer-events-none" />
 
-            <img
-              src="HeelXBanner.jpg"
-              alt="Project 1"
-              className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
-            />
+            <img src="HeelXBanner.jpg" alt="Project 1"className="w-full h-full object-cover transition duration-500 group-hover:scale-105" />
 
-            <div className="absolute inset-0 bg-white/30 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500" />
+            <div className="absolute inset-0 bg-white/30 opacity-0 group-hover:opacity-100 transition duration-500" />
 
-            <div className="absolute inset-0 flex items-center justify-center 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500 z-20">
-              <button className="bg-white text-black px-6 py-2 rounded-full 
-                                 font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 z-20">
+              <button className="bg-white text-black px-6 py-2 rounded-full font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
                 View Project
               </button>
             </div>
+        
+            <div className="absolute w-full bg-white/80 bottom-0 text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center text-black font-bold py-2">HeelX - Footwear Website</div>
+
           </div>
 
 
           {/* CARD 2 */}
           <div className="relative group md:row-span-2 overflow-hidden rounded-3xl">
 
-            <div className="absolute inset-0 border-t-[25px] border-l-[25px] 
-                            border-white/20 rounded-3xl z-10 pointer-events-none" />
+            <div className="absolute inset-0 border-t-[25px] border-l-[25px] border-white/20 rounded-3xl z-10 pointer-events-none" />
 
-            <img
-              src="ConvoX.jpg"
-              alt="Project 2"
-              className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
-            />
+            <img src="ConvoX.jpg" alt="Project 2" className="w-full h-full object-cover transition duration-500 group-hover:scale-105" />
 
-            <div className="absolute inset-0 bg-white/30 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500" />
+            <div className="absolute inset-0 bg-white/30 opacity-0 group-hover:opacity-100 transition duration-500" />
 
-            <div className="absolute inset-0 flex items-center justify-center 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500 z-20">
-              <button className="bg-white text-black px-6 py-2 rounded-full 
-                                 font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 z-20">
+              <button className="bg-white text-black px-6 py-2 rounded-full font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
                 View Project
               </button>
             </div>
+
+            <div className="absolute w-full bg-white/80 bottom-0 text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center text-black font-bold py-2">ConvoX - Chat Application</div>
+
           </div>
 
 
           {/* CARD 3 */}
           <div className="relative group md:row-span-2 overflow-hidden rounded-3xl">
 
-            <div className="absolute inset-0 border-t-[25px] border-l-[25px] 
-                            border-orange-400/20 rounded-3xl z-10 pointer-events-none" />
+            <div className="absolute inset-0 border-t-[25px] border-l-[25px] border-orange-400/20 rounded-3xl z-10 pointer-events-none" />
 
-            <img
-              src="PlanX.jpg"
-              alt="Project 3"
-              className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
-            />
+            <img src="PlanX.jpg" alt="Project 3" className="w-full h-full object-cover transition duration-500 group-hover:scale-105" />
 
-            <div className="absolute inset-0 bg-white/30 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500" />
+            <div className="absolute inset-0 bg-white/30 opacity-0 group-hover:opacity-100 transition duration-500" />
 
-            <div className="absolute inset-0 flex items-center justify-center 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500 z-20">
-              <button className="bg-white text-black px-6 py-2 rounded-full 
-                                 font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 z-20">
+              <button className="bg-white text-black px-6 py-2 rounded-full font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
                 View Project
               </button>
             </div>
+
+            <div className="absolute w-full bg-white/80 bottom-0 text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center text-black font-bold py-2">PlanX - Work Organiser Website</div>
+            
           </div>
 
 
           {/* CARD 4 */}
-          <div className="relative group md:row-span-3 md:col-start-2 md:row-start-3 
-                          overflow-hidden rounded-3xl">
+          <div className="relative group md:row-span-3 md:col-start-2 md:row-start-3 overflow-hidden rounded-3xl">
 
-            <div className="absolute inset-0 border-t-[25px] border-l-[25px] 
-                            border-white/20 rounded-3xl z-10 pointer-events-none" />
+            <div className="absolute inset-0 border-t-[25px] border-l-[25px] border-white/20 rounded-3xl z-10 pointer-events-none" />
 
-            <img
-              src="ChatBot.jpg"
-              alt="Project 4"
-              className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
+            <img src="ChatBot.jpg" alt="Project 4" className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
             />
 
-            <div className="absolute inset-0 bg-white/30 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500" />
+            <div className="absolute inset-0 bg-white/30 opacity-0 group-hover:opacity-100 transition duration-500" />
 
-            <div className="absolute inset-0 flex items-center justify-center 
-                            opacity-0 group-hover:opacity-100 
-                            transition duration-500 z-20">
-              <button className="bg-white text-black px-6 py-2 rounded-full 
-                                 font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-500 z-20">
+              <button className="bg-white text-black px-6 py-2 rounded-full font-semibold shadow-lg hover:scale-105 transition cursor-pointer">
                 View Project
               </button>
             </div>
+            <div className="absolute w-full bg-white/80 bottom-0 text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center text-black font-bold py-2">BotX - Chat Bot</div>
           </div>
 
         </div>
